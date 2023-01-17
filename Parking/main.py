1ca8b7fd32b0477f6f4391c57af1169482075b0b
Ana Pilar Barba López <pilarbarba03@gmail.com>
2023-01-17 12:41:34 +0100

    Iniciando la clase main

---
+++ Parking/main.py
@@ -0,0 +1,18 @@
+#Deberé de crear en el main un menú en el que se pregunte al usuario
+#donde quiere entrar, en la zona administrador o en la zona cliente para
+#utilizar indescritivamente los métodos propios de cada zona
+
+import Parking.Cliente.Parking
+import Parking.Cliente.Abonados
+import Parking.Cliente.Ticket
+
+import configparser
+configparser.__name__ == '__main__'
+
+
+def cliente_o_administrador():
+    
+    pregunta = int(input('Introduzca a que área desea entras: '))
+    
+    if 
+
